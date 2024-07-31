1dfe35614af61f869a84092dd65b20c08e88636d
LyLe Jones ( Zack Draco ) <132470113+sargatanas259@users.noreply.github.com>
2024-07-31 13:55:00 -0500

    Add files via upload

---
+++ decimal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double a,b,c,d,e;
+
+
+    cout<< "Please type in five decimal numbers";
+    cin>> a>>b>>c>>d>>e ;
+
+    int x = a+b+c+d+e;
+    cout<< x;
+
+
+    return 0;
+}
